ab1be597b35fe16fdf62e6132d681b6ddf9551ea
Era Mullahasani Dula <era.mullahasani_dula@kcl.ac.uk>
2021-07-16 20:29:55 +0100

    Add navbar icons + basic name logo

--- my-website/src/components/navbar/Navbar.jsx
+++ my-website/src/components/navbar/Navbar.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import "./navbar.scss"
+import githubLogo from '../../images/githubLogo.png';
+import linkedinLogo from '../../images/linkedinLogo.png';
 
 export default function Navbar() {
   return (
-    <div className="navbar">
-      
+    <div className="navbar" id="navbar">
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro" className="logo">Era Dula.</a>
+          <div className="itemContainer">
+            <img src={githubLogo} alt="Logo" className="icon"/>
+          </div>
+          <div className="itemContainer">
+            <img src={linkedinLogo} alt="Logo" className="icon"/>
+          </div>
+        </div>
+        <div className="right"></div>
+      </div>
     </div>
   )
 }
