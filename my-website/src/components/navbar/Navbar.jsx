133a1f7f7781924d38d4a3235c17a82958dbb258
Era Mullahasani Dula <era.mullahasani_dula@kcl.ac.uk>
2021-09-02 19:39:40 +0100

    Add animation to menu selection

--- my-website/src/components/navbar/Navbar.jsx
+++ my-website/src/components/navbar/Navbar.jsx
@@ -3,9 +3,9 @@ import "./navbar.scss"
 import githubLogo from '../../images/githubLogo.png';
 import linkedinLogo from '../../images/linkedinLogo.png';
 
-export default function Navbar() {
+export default function Navbar({ menuOpen, setMenuOpen }) {
   return (
-    <div className="navbar" id="navbar">
+    <div className={"navbar " + (menuOpen && "active")} id="navbar">
       <div className="wrapper">
         <div className="left">
           <a href="#intro" className="logo">Era Dula.</a>
@@ -16,7 +16,13 @@ export default function Navbar() {
             <a href="https://uk.linkedin.com/in/era-dula-95939b196" target="_blank"><img src={linkedinLogo} alt="Logo" className="icon"/></a>
           </div>
         </div>
-        <div className="right"></div>
+        <div className="right">
+          <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
+        </div>
       </div>
     </div>
   )
