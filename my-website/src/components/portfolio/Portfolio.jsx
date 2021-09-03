fcc594f4b187a32f03506fd52d1028d3d3b260f1
Era Mullahasani Dula <era.mullahasani_dula@kcl.ac.uk>
2021-09-03 23:12:50 +0100

    Add portfolio slider with basic left section

--- my-website/src/components/portfolio/Portfolio.jsx
+++ my-website/src/components/portfolio/Portfolio.jsx
@@ -4,7 +4,23 @@ import "./portfolio.scss"
 export default function Portfolio() {
   return (
     <div className="portfolio" id="portfolio">
-      
+      <div className="slider">
+        <div className="container">
+          <div className="item">
+            <div className="left">
+              <div className="leftContainer">
+                <h1>Title</h1>
+                <p>description</p>
+                <br></br>
+                <h5>Type:</h5>
+              </div>
+            </div>
+            <div className="right">
+
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
