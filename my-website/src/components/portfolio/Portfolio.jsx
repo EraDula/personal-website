b1719fd5da2e955fe7b0ceaecef0eecf0e61444f
Era Mullahasani Dula <era.mullahasani_dula@kcl.ac.uk>
2021-09-04 17:23:01 +0100

    Add dummy data + slider functionality

--- my-website/src/components/portfolio/Portfolio.jsx
+++ my-website/src/components/portfolio/Portfolio.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import "./portfolio.scss"
+import { useState } from 'react'
 
 export default function Portfolio() {
+  const [currentSlide, setCurrentSlide] = useState(0)
+
+  const handleClick = (way) => {
+    way === "left" ? setCurrentSlide(currentSlide > 0 ? currentSlide - 1 : 3) :
+      setCurrentSlide(currentSlide < 3 ? currentSlide + 1 : 0)
+  }
   return (
     <div className="portfolio" id="portfolio">
-      <div className="slider">
+      <div className="slider" style={{transform: `translateX(-${currentSlide * 100}vw)`}}>
         <div className="container">
           <div className="item">
             <div className="left">
@@ -25,9 +32,29 @@ export default function Portfolio() {
             </div>
           </div>
         </div>
+        <div className="container">
+          <div className="item">
+            <div className="left">
+              <div className="leftContainer">
+                <h1>Title2</h1>
+                <p>lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum</p>
+                <br></br>
+                <h5>Type:</h5>
+              </div>
+            </div>
+            <div className="right">
+              <div className="imgContainer">
+                <img src="assets/javascriptIcon.png" alt="" />
+                <img src="assets/reactIcon.png" alt="" />
+                <img src="assets/htmlIcon.png" alt="" />
+                <img src="assets/sassIcon.png" alt=""/>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <img src="assets/arrowDown.png" className="arrow left" alt="" />
-      <img src="assets/arrowDown.png" className="arrow right" alt=""/>
+      <img src="assets/arrowDown.png" className="arrow left" alt="" onClick={()=>handleClick("left")}/>
+      <img src="assets/arrowDown.png" className="arrow right" alt="" onClick={()=>handleClick("right")}/>
     </div>
   )
 }
