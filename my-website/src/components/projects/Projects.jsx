8d645884c566bc8d622c45faff1ae04fca3fb903
Era Mullahasani Dula <era.mullahasani_dula@kcl.ac.uk>
2021-09-05 00:32:25 +0100

    Clean up projects section + edit navbar

--- my-website/src/components/projects/Projects.jsx
+++ my-website/src/components/projects/Projects.jsx
@@ -1,24 +1,33 @@
-import React from 'react'
-import "./projects.scss"
-import { useState } from 'react'
+import React from "react";
+import "./projects.scss";
+import { useState } from "react";
 
 export default function Projects() {
-  const [currentSlide, setCurrentSlide] = useState(0)
+  const [currentSlide, setCurrentSlide] = useState(0);
 
   const handleClick = (way) => {
-    way === "left" ? setCurrentSlide(currentSlide > 0 ? currentSlide - 1 : 3) :
-      setCurrentSlide(currentSlide < 3 ? currentSlide + 1 : 0)
-  }
-  // CHANGE DUMMY DATA
+    way === "left"
+      ? setCurrentSlide(currentSlide > 0 ? currentSlide - 1 : 3)
+      : setCurrentSlide(currentSlide < 3 ? currentSlide + 1 : 0);
+  };
+
   return (
     <div className="projects" id="projects">
-      <div className="slider" style={{transform: `translateX(-${currentSlide * 100}vw)`}}>
+      <div
+        className="slider"
+        style={{ transform: `translateX(-${currentSlide * 100}vw)` }}
+      >
         <div className="container">
           <div className="item">
             <div className="left">
               <div className="leftContainer">
                 <h1>Personal Website</h1>
-                <p>Web app about myself, which I decided to make not only to share about myself but to improve my skills in React. I have also picked up new styling skills by using Sass rather than CSS.</p>
+                <p>
+                  Web app about myself, which I decided to make not only to
+                  share about myself but to improve my skills in React. I have
+                  also picked up new styling skills by using Sass rather than
+                  CSS.
+                </p>
                 <br></br>
                 <h5>Type: Web-app (Available on my GitHub)</h5>
               </div>
@@ -28,7 +37,7 @@ export default function Projects() {
                 <img src="assets/javascriptIcon.png" alt="" />
                 <img src="assets/reactIcon.png" alt="" />
                 <img src="assets/htmlIcon.png" alt="" />
-                <img src="assets/sassIcon.png" alt=""/>
+                <img src="assets/sassIcon.png" alt="" />
               </div>
             </div>
           </div>
@@ -38,7 +47,12 @@ export default function Projects() {
             <div className="left">
               <div className="leftContainer">
                 <h1>Snap-sense</h1>
-                <p>A web-app developed as a group project for hospitals (the client) to allow doctors and patients to communicate and recieve updates, notifications and the lastest information from eachother real time using Reacts responsiveness.  </p>
+                <p>
+                  A web-app developed as a group project for hospitals (the
+                  client) to allow doctors and patients to communicate and
+                  recieve updates, notifications and the lastest information
+                  from eachother real time using Reacts responsiveness.{" "}
+                </p>
                 <br></br>
                 <h5>Type: Web-app</h5>
               </div>
@@ -57,7 +71,14 @@ export default function Projects() {
             <div className="left">
               <div className="leftContainer">
                 <h1>AirBnB-like application</h1>
-                <p>An application built as a group which allowed users to select boroughs in London and view available properties, their statistics, where-abouts on a map using Google's API and allowed user's to favourite options to view after for convenience! JavaFX and sceneBuilder were used to build this application.</p>
+                <p>
+                  An application built as a group which allowed users to select
+                  boroughs in London and view available properties, their
+                  statistics, where-abouts on a map using Google's API and
+                  allowed user's to favourite options to view after for
+                  convenience! JavaFX and sceneBuilder were used to build this
+                  application.
+                </p>
                 <br></br>
                 <h5>Type: Desktop app</h5>
               </div>
@@ -75,7 +96,13 @@ export default function Projects() {
             <div className="left">
               <div className="leftContainer">
                 <h1>Teacher-Student-Track</h1>
-                <p>A web app I built during A-levels for teachers to keep track of their students progress throughout the year, by recording results, displaying graphs and tables, and grading using an automatically generated grade boundary based on relative performance. The graphs displayed used Google's API. </p>
+                <p>
+                  A web app I built during A-levels for teachers to keep track
+                  of their students progress throughout the year, by recording
+                  results, displaying graphs and tables, and grading using an
+                  automatically generated grade boundary based on relative
+                  performance. The graphs displayed used Google's API.{" "}
+                </p>
                 <br></br>
                 <h5>Type: Web-app (Available on my GitHub)</h5>
               </div>
@@ -90,8 +117,18 @@ export default function Projects() {
           </div>
         </div>
       </div>
-      <img src="assets/arrowDown.png" className="arrow left" alt="" onClick={()=>handleClick("left")}/>
-      <img src="assets/arrowDown.png" className="arrow right" alt="" onClick={()=>handleClick("right")}/>
+      <img
+        src="assets/arrowDown.png"
+        className="arrow left"
+        alt=""
+        onClick={() => handleClick("left")}
+      />
+      <img
+        src="assets/arrowDown.png"
+        className="arrow right"
+        alt=""
+        onClick={() => handleClick("right")}
+      />
     </div>
-  )
+  );
 }
