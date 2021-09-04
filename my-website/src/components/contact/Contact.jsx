1d4393acfd10b820595ad98da080d1e08254cbc5
Era Mullahasani Dula <era.mullahasani_dula@kcl.ac.uk>
2021-09-04 17:42:04 +0100

    Add contact icons

--- my-website/src/components/contact/Contact.jsx
+++ my-website/src/components/contact/Contact.jsx
@@ -4,7 +4,10 @@ import "./contact.scss"
 export default function Contact() {
   return (
     <div className="contact" id="contact">
-      
+      <div className="left">
+        <img src="assets/mailIcon.png" alt=""/>
+      </div>
+      <div className="right"></div>
     </div>
   )
 }
