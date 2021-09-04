0e49293314a15c89672ccd3bebf35990c49669f6
Era Mullahasani Dula <era.mullahasani_dula@kcl.ac.uk>
2021-09-04 18:04:04 +0100

    style the contact left section

--- my-website/src/components/contact/Contact.jsx
+++ my-website/src/components/contact/Contact.jsx
@@ -5,9 +5,17 @@ export default function Contact() {
   return (
     <div className="contact" id="contact">
       <div className="left">
-        <img src="assets/mailIcon.png" alt=""/>
+        <h1>Get in touch.</h1>
+        <span>My inbox is always open whether you have any questions, opportunities or just want to say hi, I'll try my best to respond!</span>
+        <img src="assets/handshakeIcon.png" alt=""/>
       </div>
-      <div className="right"></div>
+      <div className="right">
+        <form>
+          <input type="text" placeholder="Email" />
+          <textarea placeholder="Message"></textarea>
+          <button type="submit">Send</button>
+        </form>
+      </div>
     </div>
   )
 }
