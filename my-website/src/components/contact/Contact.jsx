a1ede28d241b832b286812fadb6734b58a3eef30
Era Mullahasani Dula <era.mullahasani_dula@kcl.ac.uk>
2021-09-04 18:25:42 +0100

    Add feedback message on submit

--- my-website/src/components/contact/Contact.jsx
+++ my-website/src/components/contact/Contact.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import "./contact.scss"
+import { useState } from 'react';
 
 export default function Contact() {
+  const [message, setMessage] = useState(false)
 
-  
   const handleSubmit = (e) => {
     e.preventDefault();
+    setMessage(true)
   }
   return (
     <div className="contact" id="contact">
@@ -19,6 +21,7 @@ export default function Contact() {
           <input type="text" placeholder="Email" />
           <textarea placeholder="Message"></textarea>
           <button type="submit">Send</button>
+          {message && <span>Thanks, I'll reply ASAP ;)</span>}
         </form>
       </div>
     </div>
