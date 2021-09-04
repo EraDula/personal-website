7185d71a8735c9b91d15eb2b5554c1853b43a45a
Era Mullahasani Dula <era.mullahasani_dula@kcl.ac.uk>
2021-09-04 23:52:40 +0100

    Remove right section + rename the divs

--- my-website/src/components/about/About.jsx
+++ my-website/src/components/about/About.jsx
@@ -4,12 +4,12 @@ import "./about.scss"
 export default function About() {
   return (
     <div className="about" id="about">
-      <div className="left">
+      <div className="aboutMeBox">
         <h1>About me.</h1>
         <h2>--------------------</h2>
-        <span>Hello! I'm Era and I'm a 3rd year student studying Computer Science. As a software devoloper I am particularly interested in web development and have had the most experience with them too. I enjoy not only the coding element but also the design aspects that come with it. That being said I also have experience in backend programming using languages such as SQL. In the near future I would like to work on a mobile application to expand my knowledge with not only technologies but the problems that could come with it.  </span>
+        <span>Hello! I'm Era and I'm a 3rd year student studying Computer Science. As a software devoloper I am particularly interested in web development and have had the most experience in this field too. I enjoy not only the coding element but also the design aspects that come with it. </span>
         <br></br>
-        <span>Currently looking for an internship where I can expand my knowledge and add to my skillset. Below are listed recent technologies I have used: </span>
+        <span>Currently looking for an internship where I can expand my knowledge and add to my skillset. Below are listed technologies I have used starting from the most recent: </span>
           <ul>
             <li>HTML</li>
             <li>CSS</li>
@@ -17,10 +17,8 @@ export default function About() {
             <li>javascript</li>
             <li>React</li>
             <li>Python</li>
+            <li>java</li>
           </ul>
-      </div>
-      <div className="right">
-          <img />
       </div>
     </div>
   )
