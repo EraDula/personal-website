e76bd0e16a9abb1d8a698742b4a6855f667f9670
Era Mullahasani Dula <era.mullahasani_dula@kcl.ac.uk>
2021-09-05 00:24:31 +0100

    Change dummy data to real data for projects

--- my-website/src/components/projects/Projects.jsx
+++ my-website/src/components/projects/Projects.jsx
@@ -17,10 +17,10 @@ export default function Projects() {
           <div className="item">
             <div className="left">
               <div className="leftContainer">
-                <h1>Title</h1>
-                <p>lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum</p>
+                <h1>Personal Website</h1>
+                <p>Web app about myself, which I decided to make not only to share about myself but to improve my skills in React. I have also picked up new styling skills by using Sass rather than CSS.</p>
                 <br></br>
-                <h5>Type:</h5>
+                <h5>Type: Web-app (Available on my GitHub)</h5>
               </div>
             </div>
             <div className="right">
@@ -37,10 +37,10 @@ export default function Projects() {
           <div className="item">
             <div className="left">
               <div className="leftContainer">
-                <h1>Title2</h1>
-                <p>lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum</p>
+                <h1>Snap-sense</h1>
+                <p>A web-app developed as a group project for hospitals (the client) to allow doctors and patients to communicate and recieve updates, notifications and the lastest information from eachother real time using Reacts responsiveness.  </p>
                 <br></br>
-                <h5>Type:</h5>
+                <h5>Type: Web-app</h5>
               </div>
             </div>
             <div className="right">
@@ -48,7 +48,6 @@ export default function Projects() {
                 <img src="assets/javascriptIcon.png" alt="" />
                 <img src="assets/reactIcon.png" alt="" />
                 <img src="assets/htmlIcon.png" alt="" />
-                <img src="assets/sassIcon.png" alt=""/>
               </div>
             </div>
           </div>
@@ -57,18 +56,16 @@ export default function Projects() {
           <div className="item">
             <div className="left">
               <div className="leftContainer">
-                <h1>Title</h1>
-                <p>lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum</p>
+                <h1>AirBnB-like application</h1>
+                <p>An application built as a group which allowed users to select boroughs in London and view available properties, their statistics, where-abouts on a map using Google's API and allowed user's to favourite options to view after for convenience! JavaFX and sceneBuilder were used to build this application.</p>
                 <br></br>
-                <h5>Type:</h5>
+                <h5>Type: Desktop app</h5>
               </div>
             </div>
             <div className="right">
               <div className="imgContainer">
-                <img src="assets/javascriptIcon.png" alt="" />
-                <img src="assets/reactIcon.png" alt="" />
-                <img src="assets/htmlIcon.png" alt="" />
-                <img src="assets/sassIcon.png" alt=""/>
+                <img src="assets/javaIcon.png" alt="" />
+                <img src="assets/sceneBuilderIcon.png" alt="" />
               </div>
             </div>
           </div>
@@ -77,18 +74,17 @@ export default function Projects() {
           <div className="item">
             <div className="left">
               <div className="leftContainer">
-                <h1>Title</h1>
-                <p>lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum</p>
+                <h1>Teacher-Student-Track</h1>
+                <p>A web app I built during A-levels for teachers to keep track of their students progress throughout the year, by recording results, displaying graphs and tables, and grading using an automatically generated grade boundary based on relative performance. The graphs displayed used Google's API. </p>
                 <br></br>
-                <h5>Type:</h5>
+                <h5>Type: Web-app (Available on my GitHub)</h5>
               </div>
             </div>
             <div className="right">
               <div className="imgContainer">
-                <img src="assets/javascriptIcon.png" alt="" />
-                <img src="assets/reactIcon.png" alt="" />
+                <img src="assets/phpIcon.png" alt="" />
                 <img src="assets/htmlIcon.png" alt="" />
-                <img src="assets/sassIcon.png" alt=""/>
+                <img src="assets/sqlIcon.png" alt="" />
               </div>
             </div>
           </div>
