80fde06a9bc59808e4278669e333b016270d9855
Era Mullahasani Dula <era.mullahasani_dula@kcl.ac.uk>
2021-09-04 23:04:52 +0100

    Add about me description - list

--- my-website/src/components/about/About.jsx
+++ my-website/src/components/about/About.jsx
@@ -7,7 +7,9 @@ export default function About() {
       <div className="left">
         <h1>About me.</h1>
         <h2>--------------------</h2>
-        <span>Basic description about me</span>
+        <span>Hello! I'm Era and I'm a 3rd year student studying Computer Science. As a software devoloper I am particularly interested in web development and have had the most experience with them too. I enjoy not only the coding element but also the design aspects that come with it. That being said I also have experience in backend programming using languages such as SQL. In the near future I would like to work on a mobile application to expand my knowledge with not only technologies but the problems that could come with it.  </span>
+        <br></br>
+        <span>Currently looking for an internship where I can expand my knowledge and add to my skillset. Below are listed recent technologies I have used: </span>
       </div>
       <div className="right">
           <img />
