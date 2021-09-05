d99cd9264b90eab7d2acd69ada7ecd89b3a87d9e
Era Mullahasani Dula <era.mullahasani_dula@kcl.ac.uk>
2021-09-06 00:17:10 +0100

    Update about me page design and info

--- my-website/src/components/about/About.jsx
+++ my-website/src/components/about/About.jsx
@@ -7,18 +7,34 @@ export default function About() {
       <div className="aboutMeBox">
         <h1>About me.</h1>
         <h2>--------------------</h2>
-        <span>Hello! I'm Era and I'm a 3rd year student studying Computer Science. As a software devoloper I am particularly interested in web development and have had the most experience in this field too. I enjoy not only the coding element but also the design aspects that come with it. </span>
-        <br></br>
-        <span>Currently looking for an internship where I can expand my knowledge and add to my skillset. Below are listed technologies I have used starting from the most recent: </span>
-          <ul>
-            <li>HTML</li>
-            <li>CSS</li>
-            <li>SASS</li>
-            <li>javascript</li>
-            <li>React</li>
-            <li>Python</li>
-            <li>java</li>
-          </ul>
+        <div className="insideBox">
+          <div className="left">
+            <h2>Interests</h2>
+            <h3>Front end</h3>
+            <h3>Back end (less experience)</h3>
+            <h3>Databases</h3>
+            <h3>Designing apps (UI/UX)</h3>
+            <h3>Web / app development</h3>
+            {/* <span>I'm a 3rd year student studying Computer Science. As a software devoloper I am particularly interested in web development and have had the most experience in this field too. I enjoy not only the coding element but also the design aspects that come with it.</span>
+            <br></br>
+            <span>Front end development allows me to use my creativity when creating applications and because of that makes it more enjoyable to me than back-end development although I have experience in back-end programming with SQL.</span>
+            <br></br>
+            <span>To the right is a list of my skills starting from the most recently used: </span> */}
+          </div>
+          <div className="right">
+            <h2>Skillset</h2>
+            <div className="skills">
+              <h3>HTML</h3>
+              <h3>CSS</h3>
+              <h3>SASS</h3>
+              <h3>javascript</h3>
+              <h3>React</h3>
+              <h3>Python</h3>
+              <h3>java</h3>
+              <h3>SQL</h3>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
